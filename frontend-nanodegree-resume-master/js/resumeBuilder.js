b6e62605776cb9ca48fc889b55b5b128a1ccc91c
小丽 <1017326027@qq.com>
2017-06-30 14:31:20 +0800

    前端（入门）电子简历完成

--- frontend-nanodegree-resume-master/js/resumeBuilder.js
+++ frontend-nanodegree-resume-master/js/resumeBuilder.js
@@ -12,83 +12,161 @@ var bio = {
 	},
 	welcomeMessage:"快乐每一天！",
 	skills:["html","css","js","jQ"],
-	//biopic:,
+	biopic:"images/fry.jpg",
 	display:function(){
 		var formattedName = HTMLheaderName.replace("%data%",bio.name);
 		var formattedRole = HTMLheaderRole.replace("%data%",bio.role);
+		var formattedMsg = HTMLwelcomeMsg.replace("%data%",bio.welcomeMessage);
+		var formattedBiopic = HTMLbioPic.replace("%data%",bio.biopic);
 		var formattedMobile = HTMLmobile.replace("%data%",bio.contacts.mobile);
 		var formattedEmail = HTMLemail.replace("%data%",bio.contacts.email);
 		var formattedGithub = HTMLgithub.replace("%data%",bio.contacts.github);
 		var formattedLocation = HTMLlocation.replace("%data%",bio.contacts.location);
-		$("#header").prepend(formattedName);
+		
 		$("#header").prepend(formattedRole);
+		$("#header").prepend(formattedName);
+		$("#header").append(formattedBiopic);
+		$("#header").append(formattedMsg);
 		$("#topContacts").append(formattedMobile);
 		$("#topContacts").append(formattedEmail);
 		$("#topContacts").append(formattedGithub);
-		$("#topContacts").append(formattedLocation);
+		$("#topContacts").append(formattedLocation);		
+		
+		if(bio.skills.length > 0){
+			$("#header").append(HTMLskillsStart);
+			for(var i = 0; i < bio.skills.length; i++){
+				var formattedSkill = HTMLskills.replace("%data%",bio.skills[i]);
+				$("#skills").append(formattedSkill);
+			}
+		}
 	}
 };
 bio.display();
-//var education = {
-//	schools:[
-//		{
-//			name:"中亚",
-//			location:"北京",
-//			degree:"初级",
-//			majors:"网站建设",
-//			dates:"2013-2014 ",
-//			url:"http://www.zhongya.com.cn/"
-//		}
-//	],
-//	onlineCourses:[
-//		{
-//			title:"Web前端",
-//			school:"udacity",
-//			dates:"Web前端",
-//			dates:"2017",
-//			url:"https://cn.udacity.com/"
-//		}
-//	],
-//	display:function(){}
-//};
-//var work = {
-//	jobs:[
-//		{
-//			employer:"香蕉广告",
-//			title:"行政前台",
-//			location:"北京",
-//			dates:"2012-2014",
-//			description:"负责公司接待客户，新员工入职，社保，办公用品、日常用品和礼品的购买，收发快递，开发票，统计报销、考勤、发票，照顾花草（爱好养花），被称为公司管家"
-//		},
-//		{
-//			employer:"香蕉广告",
-//			title:"网站工程师",
-//			location:"北京",
-//			dates:"2014-2017",
-//			description:"负责静态网站（自适应PC、IPAD、手机）、微信分享页面（手机端）、动态网站前端部分，微信公众号管理，邮箱管理，域名备案，静态网站上传到服务器，做了一段时间的客户执行"
-//		}
-//	]
-//	display:function(){}
-//};
-//var projects = {
-//	projects:[
-//		{
-//			title:"独角兽",
-//			dates:"2014",
-//			description:"做了PC、手机两版页面。PC端可根据屏幕大小自适应，手机端也是。通过PHP判断解析需要哪一版的页面",
-//			images:["images/dujiaoshou(PC).png","images/dujiaoshou(mobile).png"]
-//		},
-//		{
-//			title:"信中利",
-//			dates:"2015",
-//			description:"动态网站，向下兼容到IE8以上",
-//			images:["images/xinzhongli(index).png","images/xinzhongli(touzizuhe).png"]
-//		},
-//		{
-//			title:"香蕉广告",
-//			dates:"2016",
-//			description:"静态网站，HTML5+CSS3自适应PC+mobile",
-//			images:["images/xiangjiaoguanggao(PC).png","images/xiangjiaoguanggao(mobile).png"]
-//		}
-//	]
-//}
+
+var work = {
+	jobs:[
+		{
+			employer:"香蕉广告",
+			title:"行政前台",
+			location:"北京",
+			dates:"2012-2014",
+			description:"负责公司接待客户，新员工入职，社保，办公用品、日常用品和礼品的购买，收发快递，开发票，统计报销、考勤、发票，照顾花草（爱好养花），被称为公司管家"
+		},
+		{
+			employer:"香蕉广告",
+			title:"网站工程师",
+			location:"北京",
+			dates:"2014-2017",
+			description:"负责静态网站（自适应PC、IPAD、手机）、微信分享页面（手机端）、动态网站前端部分，微信公众号管理，邮箱管理，域名备案，静态网站上传到服务器，做了一段时间的客户执行"
+		}
+	],
+	display:function(){
+		work.jobs.forEach(function(job){
+			$("#workExperience").append(HTMLworkStart);
+			var iobEmloyer = HTMLworkEmployer.replace("%data%",job.employer);
+			var jobTitle = HTMLworkTitle.replace("%data%",job.title);
+			var formattedName = iobEmloyer + jobTitle;
+			var formattedLocation = HTMLworkLocation.replace("%data%",job.location);
+			var formattedDates = HTMLworkDates.replace("%data%",job.dates);
+			var formattedDescription = HTMLworkDescription.replace("%data%",job.description);
+			$(".work-entry:last").append(formattedName);
+			$(".work-entry:last").append(formattedLocation);
+			$(".work-entry:last").append(formattedDates);
+			$(".work-entry:last").append(formattedDescription);
+		})
+	}
+};
+work.display();
+var projects = {
+	projects:[
+		{
+			title:"独角兽",
+			dates:"2014",
+			description:"做了PC、手机两版页面。PC端可根据屏幕大小自适应，手机端也是。通过PHP判断解析需要哪一版的页面",
+			images:["images/dujiaoshou(PC).png","images/dujiaoshou(mobile).png"]
+		},
+		{
+			title:"信中利",
+			dates:"2015",
+			description:"动态网站，向下兼容到IE8以上",
+			images:["images/xinzhongli(index).png","images/xinzhongli(touzizuhe).png"]
+		},
+		{
+			title:"香蕉广告",
+			dates:"2016",
+			description:"静态网站，HTML5+CSS3自适应PC+mobile",
+			images:["images/xiangjiaoguanggao(PC).png","images/xiangjiaoguanggao(mobile).png"]
+		}
+	],
+	display:function(){
+		projects.projects.forEach(function(project){
+			$("#projects").append(HTMLprojectStart);
+			var formattedTitle = HTMLprojectTitle.replace("%data%",project.title);
+			var formattedDaes = HTMLprojectDates.replace("%data%",project.dates);
+			var formattedDescription = HTMLprojectDescription.replace("%data%",project.description);
+			$(".project-entry:last").append(formattedTitle);
+			$(".project-entry:last").append(formattedDaes);
+			$(".project-entry:last").append(formattedDescription);
+			if(project.images.length > 0){
+				for(var i = 0; i < project.images.length; i++){
+					var formattedImg = HTMLprojectImage.replace("%data%",project.images[i]);
+					$(".project-entry:last").append(formattedImg);
+				}
+			}
+		})
+	}
+};
+projects.display();
+var education = {
+	schools:[
+		{
+			name:"中亚",
+			location:"北京",
+			degree:"初级",
+			majors:"网站建设",
+			dates:"2013-2014 ",
+			url:"http://www.zhongya.com.cn/"
+		}
+	],
+	onlineCourses:[
+		{
+			title:"Web前端",
+			school:"udacity",
+			majors:"Web前端",
+			dates:"2017",
+			url:"https://cn.udacity.com/"
+		}
+	],
+	display:function(){
+		education.schools.forEach(function(school){
+			$("#education").append(HTMLschoolStart);
+			var schoolName = HTMLschoolName.replace("%data%",school.name);
+			var schoolDegre = HTMLschoolDegree.replace("%data%",school.degree);
+			var formattedName = schoolName + schoolDegre;
+			var formattedLocation = HTMLschoolLocation.replace("%data%",school.location);
+			var formattedMajors = HTMLschoolMajor.replace("%data%",school.majors);
+			var formattedDates= HTMLschoolDates.replace("%data%",school.dates);
+			var formattedUrl = HTMLonlineURL.replace("%data%",school.url);
+			$(".education-entry:last").append(formattedName);
+			$(".education-entry:last").append(formattedLocation);
+			$(".education-entry:last").append(formattedDates);
+			$(".education-entry:last").append(formattedMajors);			
+			$(".education-entry:last").append(formattedUrl);
+		});	
+		education.onlineCourses.forEach(function(online){
+			$("#education").append(HTMLonlineClasses);
+			$("#education").append(HTMLschoolStart);
+			var onlineTitle = HTMLonlineTitle.replace("%data%",online.title);
+			var onlineSchool = HTMLonlineSchool.replace("%data%",online.school);
+			var formattedName = onlineTitle + onlineSchool;
+			var formattedMajors = HTMLschoolMajor.replace("%data%",online.majors);
+			var formattedDates= HTMLonlineDates.replace("%data%",online.dates);
+			var formattedUrl = HTMLonlineURL.replace("%data%",online.url);
+			$(".education-entry:last").append(formattedName);
+			$(".education-entry:last").append(formattedDates);
+			$(".education-entry:last").append(formattedMajors);			
+			$(".education-entry:last").append(formattedUrl);
+		});
+	}
+};
+education.display();
